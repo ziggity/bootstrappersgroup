3238fb272945a4518efea94a84ffca05fe538491
Zach <17103355+ziggity@users.noreply.github.com>
2017-08-22 21:41:25 -0700

    Create apicallgenerators.js
    Following along fun fun function youtube videos

---
+++ apicallgenerators.js
@@ -0,0 +1,30 @@
+
+//$ NPM install node-fetch
+
+// yield works just as you would assume, it yields, and passes it on to the next function.
+
+const fetch = require('node-fetch')
+
+run(function *() {
+    const uri = 'http://jsonplaceholder.typicode.com/posts/1'
+    const response = yield fetch(uri)
+    const post = yield response.json()
+    const title = post.title
+    return title
+})
+.catch( error => console.error(error.stack))
+.then(x =>
+    console.log('run resulted in', x)
+)
+
+
+function run(generator){
+    const iterator = generator()
+    function iterate(iteration){
+  if(iteration.done) return iteration.value  
+  const promise = iteration.value
+  return promise.then( x => iterate(iterator.next(x)))
+}
+       return iterate(iterator.next())
+
+}
